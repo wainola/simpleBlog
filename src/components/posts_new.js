760add4652c663d77b3d96730222115662926d64
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-03-20 00:38:23 -0300

    generando el formulario con redux-form, genrando validaciones, obteniendo datos en el submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,71 @@
 import React, {Component} from 'react';
+// Component and function
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component{
-    render(){
+    renderField(field){
         return(
-            <div>
-                PostNew
+            <div className="form-group">
+                <label htmlFor="">{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                {field.meta.error}
+            </div>
+        );
+
+    }
+    onSubmit(values){
+        console.log(values);
+    }
+    render(){
+        const {handleSubmit} = this.props;
+
+        return(
+            <div className="container">
+                <form action="" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <Field
+                        label="Title"
+                        name="title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Categories"
+                        name="categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Post Content"
+                        name="content"
+                        component={this.renderField}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         );
     }
 }
 
-export default PostsNew;
+function validate(values){
+    const errors = {};
+    // validate the inputs from 'values'
+    if(!values.title){
+        errors.title = "Enter a title!";
+    }
+    if(!values.categories){
+        errors.categories = "Enter somre categories";
+    }
+    if(!values.content){
+        errors.content = "Enter sobre content please";
+    }
+    // if errors is empty, the form is fine to submit
+    // if errors has any properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
+import {reducer as fromReducer} from 'redux-form';
 import PostsReducer from './reducer_posts';
 
+// Wiring up redux form
 const rootReducer = combineReducers({
-    posts: PostsReducer
+    posts: PostsReducer,
+    form: fromReducer
 });
 
 export default rootReducer;
